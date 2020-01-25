0300d2373b94bfcd704573182e3ae697dc996984
Andreas Demenagas <ademenagas01@gmail.com>
2020-01-25 14:07:00 +0200

    Initial Commit

---
+++ InternetRules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetRules
+//
+//  Created by Andrew Demenagas on 25/1/20.
+//  Copyright © 2020 Andrew Demenagas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
