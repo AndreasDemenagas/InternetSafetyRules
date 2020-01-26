cd8be7bf01a55073c96b0c964203e373890193a8
Andreas Demenagas <ademenagas01@gmail.com>
2020-01-26 16:49:47 +0200

    Animations & Game Details Label

--- InternetRules/MainController.swift
+++ InternetRules/MainController.swift
@@ -32,6 +32,19 @@ class MainController: UIViewController {
         return lbl
     }()
     
+    let detailsLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "HelveticaNeue", size: 18)
+        lbl.textColor = .white
+        lbl.text = "This quick game will make you familiar with the 10 Internet Safety & Security Rules as presented by the Greek CSI Institute. You will be shown a number of cards. Each card contains one rule. Make sure to read them carefully to understand them completetely! Browse safely!"
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.textAlignment = .justified
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.alpha = 0
+        return lbl
+    }()
+    
     let startButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.backgroundColor = UIColor.init(r: 51, g: 153, b: 255)
@@ -40,6 +53,7 @@ class MainController: UIViewController {
         btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 19)
         btn.layer.cornerRadius = 10
         btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(handleStart), for: .touchUpInside)
         return btn
     }()
 
@@ -52,6 +66,59 @@ class MainController: UIViewController {
         
         //setupCards()
         //Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(setupBackground), userInfo: nil, repeats: false)
+    }
+    
+    @objc fileprivate func handleStart() {
+        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            
+            let padding: CGFloat = 10 + 20
+            let x = self.view.frame.width - self.subtitleLabel.frame.width - (2 * padding)
+            
+            self.subtitleLabel.transform = CGAffineTransform(translationX: x, y: 0)
+    
+        }, completion: { (_) in
+            
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                let padding: CGFloat = 10 + 20
+                let x = self.view.frame.width - self.subtitleLabel.frame.width - (2 * padding)
+                
+                self.subtitleLabel.transform = CGAffineTransform(translationX: x, y: self.view.frame.height - 200)
+                self.subtitleLabel.alpha = 0
+                
+                self.titleLabel.alpha = 0
+                self.startButton.alpha = 0
+                
+            }, completion: { (_) in
+                
+                self.presentDetailsLabel()
+                
+            })
+            
+        })
+    }
+    
+    func presentDetailsLabel() {
+    
+        UIView.animate(withDuration: 1, delay: 1, options: .curveEaseIn, animations: {
+            
+            self.detailsLabel.alpha = 1
+            
+        }) { (_) in
+            
+            Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { (_) in
+                
+                UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
+                    
+                    self.detailsLabel.alpha = 0
+                    
+                }) { (_) in
+                    
+                    print("Game intro complete...")
+                    
+                }
+            }
+        }
     }
     
     @objc fileprivate func setupBackground() {
@@ -73,13 +140,20 @@ class MainController: UIViewController {
         titleLabel.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 200, left: 20, bottom: 0, right: 20), size: .init(width: .zero, height: 50))
         
         view.addSubview(subtitleLabel)
-        subtitleLabel.anchor(top: titleLabel.bottomAnchor, leading: titleLabel.leadingAnchor, bottom: nil, trailing: titleLabel.trailingAnchor, padding: .init(top: 8, left: 10, bottom: 0, right: 0), size: .init(width: .zero, height: 25))
+        subtitleLabel.anchor(top: titleLabel.bottomAnchor, leading: titleLabel.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 8, left: 10, bottom: 0, right: 0), size: .init(width: 240, height: 25))
         
         view.addSubview(startButton)
         startButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         startButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        view.addSubview(detailsLabel)
+        detailsLabel.heightAnchor.constraint(equalToConstant: 180).isActive = true
+        detailsLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        detailsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        detailsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 }
